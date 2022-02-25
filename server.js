cf6599bfd7c771d5d8fbe432ba4e43c10d4b9f41
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-25 23:24:21 +0100

    config for cors

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express()
 const {graphqlHTTP} =  require('express-graphql')
 const schema = require('./schemas/schema')
 const mongoose  =require('mongoose')
-
+const cors = require('cors')
+app.use(cors())
 mongoose.connect('mongodb://localhost:27017/graphqlTest').then(console.log(`Conection to db is open`)).catch(err=> console.log(err))
 
 app.use('/graphql', graphqlHTTP({
