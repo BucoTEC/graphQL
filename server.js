adb8fb0e464b1804040dc3347bf20ce0beb9b21a
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-02-25 11:58:42 +0100

    added basic models

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const {graphqlHTTP} =  require('express-graphql')
 const schema = require('./schemas/schema')
+const mongoose  =require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/graphqlTest').then(console.log(`Conection to db is open`)).catch(err=> console.log(err))
 
 app.use('/graphql', graphqlHTTP({
     schema,
